2d4957ae6ad5a5ceef11e0720fb6f134e2177e92
catarinafoliveira <felixcata@gmail.com>
2024-09-13 13:16:55 +0100

    Complete PersonComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { PersonsComponent } from './persons/persons.component';
+import { MessageService } from './thisServices/message.service';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +14,14 @@ import { PersonsComponent } from './persons/persons.component';
 })
 export class AppComponent {
   title = 'myFrontend_v1';
+  message: { text: string; type: string } | null = null;
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messageService.currentMessage$.subscribe((msg) => {
+      this.message = msg;
+      setTimeout(() => {this.message = null;}, 5000);
+    });
+  }
 }
